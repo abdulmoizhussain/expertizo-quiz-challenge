18bdd4f899384765e026f807c3989aa1a7217d65
Abdul Moiz <moizhus123@gmail.com>
2020-06-20 22:09:20 +0500

    --ye to ban rha he

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import {
   Progress,
   Row,
   Col,
+  Button,
 } from 'reactstrap';
 import StarRatingComponent from 'react-star-rating-component';
 
 function App() {
   return (
-    <Row className="justify-content-md-center">
-      <Col md={8}>
+    <Row className="justify-content-md-center m-0 hv-100">
+      <Col md={8} className="app-border d-flex flex-column">
         <Row>
-          <Col>
+          <Col className="px-0">
             <Progress multi>
-              <Progress bar color="secondary" value={50} />
+              <Progress bar color="secondary" value={2} max={20} />
             </Progress>
           </Col>
         </Row>
@@ -23,7 +24,6 @@ function App() {
           <Col md={8}>
             <h1>Question 16 of 20</h1>
 
-            <br />
             <p>Entertainment board games</p>
             <StarRatingComponent
               name="rate1"
@@ -35,28 +35,53 @@ function App() {
             />
 
             <br />
+            <br />
 
             <h5>blah blah blah blah blah blah blah blah blah blah blah blah blah blah ?</h5>
 
             <br />
 
-            <Row className='align-items-end'>
-              <Col>asdfasdf</Col>
-              <Col>asdfasdf</Col>
+            <Row>
+              <Col className="m-3 d-flex justify-content-center"><Button color="light" className="btn btn-sm border border-dark">ghost</Button></Col>
+              <Col className="m-3 d-flex justify-content-center"><Button color="light" className="btn btn-sm border border-dark">ghost</Button></Col>
             </Row>
             <Row>
-              <Col>asdfasdf</Col>
-              <Col>asdfasdf</Col>
+              <Col className="m-3 d-flex justify-content-center"><Button color="light" className="btn btn-sm border border-dark" >ghost</Button></Col>
+              <Col className="m-3 d-flex justify-content-center"><Button color="light" className="btn btn-sm border border-dark" >ghost</Button></Col>
             </Row>
 
+            <Row>
+              <Col className="d-flex justify-content-center">
+                <h4>Correct!</h4>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="d-flex justify-content-center">
+                <Button color="light" className='border border-dark'>Next Question</Button>
+              </Col>
+            </Row>
           </Col>
         </Row>
-
-        <Row className='align-items-end'>
-          <Col>asdfasdf</Col>
-          <Col>asdfasdf</Col>
-          <Col>asdfasdf</Col>
-          <Col>asdfasdf</Col>
+        <Row className="mt-auto justify-content-center">
+          <Col md={8}>
+            <Row>
+              <Col className="text-left">
+                <strong>
+                  Score: 67%
+                </strong>
+              </Col>
+              <Col className="text-right">
+                <strong>
+                  Max Score: 75%
+                </strong>
+              </Col>
+            </Row>
+            <Progress multi className="border border-dark">
+              <Progress bar className="progress-dark" value={3} max={20} />
+              <Progress bar className="progress-normal" value={2} max={20} />
+              <Progress bar className="progress-light" value={4} max={20} />
+            </Progress>
+          </Col>
         </Row>
       </Col>
     </Row>
